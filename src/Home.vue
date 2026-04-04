d358a9bf01478264814715c0a5c24c44588fc376
Iryna Sagata <irasagata@gmail.com>
2026-04-04 23:07:14 +0200

    implement fetch

--- src/Home.vue
+++ src/Home.vue
@@ -1,3 +1,26 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue'
+import { useFetch } from './scripts/fetch'
+
+const apiBaseUrl = 'https://api.frankfurter.dev/v2'
+
+const baseCurrencies = reactive(['UAH','USD', 'EUR', 'PLN'])
+
+const targetCurrencies = reactive(['USD', 'EUR', 'PLN', 'AED'])
+
+const { data, error } = useFetch(`${apiBaseUrl}/rates?base=${baseCurrencies[0]}&quotes=${targetCurrencies.join(',')}`)
+
+</script>
+
 <template>
-  <div>This is Home</div>
+  <label for="currencies">Choose a currency:</label>
+  <select id="currencies">
+    <option v-for="currency in baseCurrencies" value="{{ currency }}">{{ currency }}</option>
+  </select>
+  <table> 
+    <tr v-for="item in data">
+      <td>{{ item.quote }}</td>
+      <td>{{ item.rate }}</td>
+    </tr>
+  </table>
 </template>
